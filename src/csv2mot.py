23a8ca57bfa12489ee319aa56ffbd9137fb62b46
minarin0179 <minarin0179@gmail.com>
2024-01-22 22:45:18 +0900

    Refactor csv2mot.py

--- src/csv2mot.py
+++ src/csv2mot.py
@@ -13,24 +13,18 @@ with open(filename, encoding='utf8', newline='') as f:
     for row in csvreader:
         frame_count += 1
         row = list(map(int,row)) # 文字列を数値に変換
-        data_body.append([row[0], *map(pos2rot,row[1:])])
+        data_body.append([row[0], *map(pos2rot,row[1:])]) # 先頭は秒数なので除外
 
-# ファイル出力
 output = [
     [JOINT_NUM, frame_count],
     *data_body
 ]
 
-
 # CSVファイルのパス
 csv_file_path = '../data/output.mot'
 
-# CSVファイルを書き込みモードで開く
 with open(csv_file_path, 'w', newline='') as csv_file:
-    # CSVライターを作成
     csv_writer = csv.writer(csv_file)
-
-    # データをCSVファイルに書き込む
     csv_writer.writerows(output)
 
 print(f'CSVファイルが作成されました: {csv_file_path}')
