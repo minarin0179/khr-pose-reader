dcee44589402b1ad4b4939d273e372ea12c44115
minarin0179 <minarin0179@gmail.com>
2024-01-22 22:39:47 +0900

    refactor

--- src/csv2mot.py
+++ src/csv2mot.py
@@ -1,31 +1,29 @@
-# CSVを読み込み
-
 import csv
 
 filename = '../data/test.csv'
 
-# 関節数を定義
-JOINT_NUM = 22
-frame_count = 0
-data = []
+JOINT_NUM = 22 # 関節数
+frame_count = 0 # キーフレーム数
+data_body = [] # データ本体
 
 with open(filename, encoding='utf8', newline='') as f:
     csvreader = csv.reader(f, delimiter=' ')
+    
+    pos2rot = lambda x: int((x - 7500) * 180 / 5300)
     for row in csvreader:
         frame_count += 1
-        row = list(map(int,row))
-        pos2rot = lambda x: (x - 7500) * (180 / 5300)
-        data.append([row[0], *map(pos2rot,row[1:])])
+        row = list(map(int,row)) # 文字列を数値に変換
+        data_body.append([row[0], *map(pos2rot,row[1:])])
 
 # ファイル出力
 output = [
     [JOINT_NUM, frame_count],
-    *data
+    *data_body
 ]
 
 
 # CSVファイルのパス
-csv_file_path = '../data/output.csv'
+csv_file_path = '../data/output.mot'
 
 # CSVファイルを書き込みモードで開く
 with open(csv_file_path, 'w', newline='') as csv_file:
