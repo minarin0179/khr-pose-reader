b29cb34aea2f0bf82046798acec25e1b1cfe8a10
minarin0179 <minarin0179@gmail.com>
2024-01-22 20:06:20 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 # /bin/bash
-curl -o Rcb4Lib.zip https://kondo-robot.com/w/wp-content/uploads/RCB4Lib_for_Python_V100B.zip
-unzip Rcb4Lib.zip
+
+curl -O https://kondo-robot.com/w/wp-content/uploads/RCB4Lib_for_Python_V100B.zip
+unzip RCB4Lib_for_Python_V100B.zip
+cp -r RCB4Lib_for_Python_V100B/Rcb4Lib .
+rm -rf RCB4Lib_for_Python_V100B
+rm RCB4Lib_for_Python_V100B.zip
