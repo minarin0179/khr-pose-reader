d8a070e9cf77e5e2f3a3c5d6886ca45a67b83fd5
minarin0179 <minarin0179@gmail.com>
2024-01-23 15:48:20 +0000

    lint

--- src/csv2mot.py
+++ src/csv2mot.py
@@ -1,30 +1,26 @@
 import csv
 
-filename = '../data/test.csv'
+filename = "../data/test.csv"
 
-JOINT_NUM = 22 # 関節数
-frame_count = 0 # キーフレーム数
-data_body = [] # データ本体
+JOINT_NUM = 22  # 関節数
+frame_count = 0  # キーフレーム数
+data_body = []  # データ本体
 
-with open(filename, encoding='utf8', newline='') as f:
-    csvreader = csv.reader(f, delimiter=' ')
-    
+with open(filename, encoding="utf8", newline="") as f:
+    csvreader = csv.reader(f, delimiter=" ")
     pos2rot = lambda x: int((x - 7500) * 180 / 5300)
     for row in csvreader:
         frame_count += 1
-        row = list(map(int,row)) # 文字列を数値に変換
-        data_body.append([row[0], *map(pos2rot,row[1:])]) # 先頭は秒数なので除外
+        row = list(map(int, row))  # 文字列を数値に変換
+        data_body.append([row[0], *map(pos2rot, row[1:])])  # 先頭は秒数なので除外
 
-output = [
-    [JOINT_NUM, frame_count],
-    *data_body
-]
+output = [[JOINT_NUM, frame_count], *data_body]
 
 # CSVファイルのパス
-csv_file_path = '../data/output.mot'
+csv_file_path = "../data/output.mot"
 
-with open(csv_file_path, 'w', newline='') as csv_file:
+with open(csv_file_path, "w", newline="") as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerows(output)
 
-print(f'CSVファイルが作成されました: {csv_file_path}')
+print(f"CSVファイルが作成されました: {csv_file_path}")
