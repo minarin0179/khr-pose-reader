cc4b3d322cdddd23ab4f02ce650bc94d8e1eb8ed
fjktkm <fjktkm@gmail.com>
2024-01-23 04:27:50 +0000

    refactor: コマンド間の依存関係の見通しが立ちやすいように修正

--- setup.sh
+++ setup.sh
@@ -12,12 +12,10 @@ pip install -r requirements.txt
 
 # RCB4Lib
 mkdir -p lib
-cd lib
-curl -O https://kondo-robot.com/w/wp-content/uploads/RCB4Lib_for_Python_V100B.zip
-unzip RCB4Lib_for_Python_V100B.zip
-cp -r RCB4Lib_for_Python_V100B/Rcb4Lib/* .
-rm -rf RCB4Lib_for_Python_V100B
-rm RCB4Lib_for_Python_V100B.zip
-black Rcb4BaseLib.py # インデントに tab が混ざっているのを修正
-sed -i 's/return 0, Rcb4BaseLib.CmdOkType.Error.value, buf/return 0, Rcb4BaseLib.CmdOkType.Error.value, txbuf/' Rcb4BaseLib.py # 変数名のミスを修正
-cd ..
+curl -o lib/RCB4Lib.zip https://kondo-robot.com/w/wp-content/uploads/RCB4Lib_for_Python_V100B.zip
+unzip lib/RCB4Lib.zip -d lib && rm lib/RCB4Lib.zip
+cp -r lib/RCB4Lib_for_Python_V100B/Rcb4Lib/* lib/ && rm -rf lib/RCB4Lib_for_Python_V100B
+
+# RCB4Lib の修正
+black lib/Rcb4BaseLib.py # インデントに含まれる Tab を Space に修正
+sed -i 's/return 0, Rcb4BaseLib.CmdOkType.Error.value, buf/return 0, Rcb4BaseLib.CmdOkType.Error.value, txbuf/' lib/Rcb4BaseLib.py # 変数名のミスを修正
