56fad02b849560438198955f524b241164ec0f3d
minarin0179 <minarin0179@gmail.com>
2024-01-27 05:30:06 +0000

    csvからmotへのindexの変換を実装

--- src/csv2mot.py
+++ src/csv2mot.py
@@ -1,4 +1,5 @@
 import csv
+import khr_structure
 
 filename = "../data/test.csv"
 
@@ -6,15 +7,46 @@ JOINT_NUM = 22  # 関節数
 frame_count = 0  # キーフレーム数
 data_body = []  # データ本体
 
+indecies = [
+    khr_structure.CSV_STRUCTURE.index(element)
+    for element in khr_structure.MOT_STRUCTURE
+]  # csvからmotへの変換用のインデックス
+
+
+def pos2rot(pos: int) -> int:
+    """サーボモーターの位置を角度に変換する
+
+    Args:
+        pos (int): サーボモーターの位置
+
+    Returns:
+        int: 角度
+    """
+    return round((pos - 7500) * 180 / 5300)
+
+
+def csv_to_mot(data: list[str]) -> list[int]:
+    """csvのデータをmotのデータに変換する
+
+    Args:
+        data (list[str]): csvのデータ
+
+    Returns:
+        list[int]: motのデータ
+    """
+    data = list(map(int, data))  # 文字列を数値に変換
+    time, positions = data[0], data[1:]
+    positions = [positions[i] for i in indecies]
+    rotations = map(pos2rot, positions)
+    return [time, *rotations]
+
+
 with open(filename, encoding="utf8", newline="") as f:
     csvreader = csv.reader(f, delimiter=" ")
-    pos2rot = lambda x: int((x - 7500) * 180 / 5300)
     for row in csvreader:
         frame_count += 1
-        row = list(map(int, row))  # 文字列を数値に変換
-        data_body.append([row[0], *map(pos2rot, row[1:])])  # 先頭は秒数なので除外
+        data_body.append(csv_to_mot(row))
 
-output = [[JOINT_NUM, frame_count], *data_body]
 
 # CSVファイルのパス
 csv_file_path = "../data/output.mot"
