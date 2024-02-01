db3c5fc56af0f8330ee20fd4161ac690c4f05e6e
fjktkm <fjktkm@gmail.com>
2024-02-01 14:51:02 +0900

    feat: ポジションを設定するサンプルコードを追加

---
+++ src/set_pos.py
@@ -0,0 +1,38 @@
+# coding: UTF-8
+import platform
+from Rcb4BaseLib import Rcb4BaseLib  # Rcb4BaseLib.pyの中のRcb4BaseLibが使えるように設定
+
+DEVICE_NAME_WIN = "COM3"
+DEVICE_NAME_LINUX = "/dev/ttyUSB0"
+BUNDRATE = 1250000  # ボーレート
+TIMEOUT = 1.3  # タイムアウト(s)
+FRAME_INTERVAL = 500
+SIO1_4 = 0x01
+SIO5_8 = 0x02
+
+
+# サーボモーターのIDと位置を設定する関数
+def set_servo_position(rcb4, servo_id, position, frame=200):
+    if not rcb4.setSingleServo(servo_id, SIO1_4, position, frame):
+        print(f"Failed to set position for servo ID {servo_id}")
+
+
+def main():
+    rcb4 = Rcb4BaseLib()  # rcb4をインスタンス(定義)
+    device_name = (
+        DEVICE_NAME_WIN if platform.system() == "Windows" else DEVICE_NAME_LINUX
+    )
+
+    # rcb4.openはcheckAcknowledgeの結果を返す
+    if not rcb4.open(device_name, BUNDRATE, TIMEOUT):
+        print("checkAcknowledge error")
+        return
+
+    # サーボモーターID 1 に位置 5000 を設定
+    set_servo_position(rcb4, 1, 7500)
+
+    rcb4.close()
+
+
+if __name__ == "__main__":
+    main()
